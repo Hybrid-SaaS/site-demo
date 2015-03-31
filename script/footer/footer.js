dc10e151092513d29e4ec1eb57d9586ab13a221b
Rick Littel <rick@hybridsaas.com>
2015-03-31 12:36:01 +0200

    Functie footer altijd bottom pagina

---
+++ script/footer/footer.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function() {
+	var windowHeight = $(window).height();
+	var testHeight = $(".test-mode").first().outerHeight();
+	var headerHeight = $("#header").outerHeight();
+	var bottomHeight = $("#bottommenu").outerHeight();
+	var bottomHeight2 = $("#bottommenu2").outerHeight();
+	var minheight = windowHeight - (testHeight + headerHeight + bottomHeight + bottomHeight2);
+	$("#maincontent").css("min-height", minheight + "px");
+});
