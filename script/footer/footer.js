519c1728e0aafdb6fba903de6296b4277f675a9d
Rick Littel <rick@hybridsaas.com>
2015-03-31 13:25:05 +0200

    Aanpassing footer script

--- script/footer/footer.js
+++ script/footer/footer.js
@@ -1,9 +1,15 @@
 ﻿$(document).ready(function() {
 	var windowHeight = $(window).height();
-	var testHeight = $(".test-mode").first().outerHeight();
-	var headerHeight = $("#header").outerHeight();
-	var bottomHeight = $("#bottommenu").outerHeight();
-	var bottomHeight2 = $("#bottommenu2").outerHeight();
-	var minheight = windowHeight - (testHeight + headerHeight + bottomHeight + bottomHeight2);
-	$("#maincontent").css("min-height", minheight + "px");
+	var testHeight = $(".test-mode").first().outerHeight(true);
+	var headerHeight = $("#header").outerHeight(true);
+	var bottomHeight = $("#bottommenu").outerHeight(true);
+	var bottomHeight2 = $("#bottommenu2").outerHeight(true);
+	var diff = $(".maincontent").first().outerHeight(true) - $(".maincontent").first().height();
+	var minheight = testHeight + headerHeight + bottomHeight + bottomHeight2 + diff + 10;
+	$(".maincontent").first().css("min-height", (windowHeight - minheight) + "px");
+
+	$(window).resize(function () {
+		windowHeight = $(window).height();
+		$(".maincontent").first().css("min-height", (windowHeight - minheight) + "px");
+	});
 });
