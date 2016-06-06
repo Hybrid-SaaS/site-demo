54c436c756edcef472eec3b8648651e01d54cd42
Jeffrey van der Stad <jeffrey@hybridsaas.com>
2016-06-06 13:04:05 +0200

    Volgorde

--- script/product/product-v3.js
+++ script/product/product-v3.js
@@ -9,6 +9,13 @@
 		}
 	}
 
+	var guid = WebPage.Data.productGuid;
+	$("#header .content").append("<div title='Ga naar uw winkelwagen' class='cart-wrap' id='shoppingCart'>" +
+		"<div class='image'><img class='cart' style='width: 48px; height: 48px;' alt='Winkelwagen' src='https://raw.githubusercontent.com/Hybrid-SaaS/site-demo/master/images/basket_48.png'></div>" +
+		"<div class='text'><div id='cart-count'><span id='shoppingcart_amount'>0</span> product<span id='shoppingcart_text'>en</span></div><div>" +
+		"€ <span id='shoppingcart_total'>0.00</span></div></div></div>").addClass("clearfix");
+	$(".body .intro.item").append("<div id='basketAddAmount'><div class='label'>Aantal:</div><input id='basketAmount' type='number' value='1' min='1' /></div>");
+
 	//voorraad:
 	if (WebPage.Data.respectStock)
 	{
@@ -31,14 +38,6 @@
 			});
 		}
 	}
-
-
-	var guid = WebPage.Data.productGuid;
-	$("#header .content").append("<div title='Ga naar uw winkelwagen' class='cart-wrap' id='shoppingCart'>" +
-		"<div class='image'><img class='cart' style='width: 48px; height: 48px;' alt='Winkelwagen' src='https://raw.githubusercontent.com/Hybrid-SaaS/site-demo/master/images/basket_48.png'></div>" +
-		"<div class='text'><div id='cart-count'><span id='shoppingcart_amount'>0</span> product<span id='shoppingcart_text'>en</span></div><div>" +
-		"€ <span id='shoppingcart_total'>0.00</span></div></div></div>").addClass("clearfix");
-	$(".body .intro.item").append("<div id='basketAddAmount'><div class='label'>Aantal:</div><input id='basketAmount' type='number' value='1' min='1' /></div>");
 
 	$("#submit.cart-button").one("click", function () {
 		var amount = $("#basketAmount").val();
